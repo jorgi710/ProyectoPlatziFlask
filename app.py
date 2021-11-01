5b0309cb77e855cdd309fb03e9179289d0bfc46b
DS Andres Ayalagit <jorgi710@hotmail.com>
2021-10-31 19:03:52 -0500

    Primer commit Andres

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    context = {
+        'error': error,
+    }
+    return render_template('404.html', **context)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    context = {
+        'error': error,
+    }
+    return render_template('500.html', **context)
+
+
+todos = ["todo1", "todo2", "todo3"]
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+    return response
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():  # put application's code here
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+        'login_form': login_form,
+        'username': username,
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con éxito!!!')
+
+        return redirect(url_for('index'))
+
+    return render_template('hello.html', **context)
+
+
+if __name__ == '__main__':
+    app.run()
